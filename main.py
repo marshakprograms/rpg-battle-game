7e6a8845403b5ed2db044bd92fe9be28f293973d
Marsha Shearin <marshashearin@marshakdesigns.com>
2026-04-29 16:01:22 -0400

    Potions option added

--- main.py
+++ main.py
@@ -12,40 +12,62 @@ class Character:
         target.health -= self.attack_power
         print(f"{self.name} attacks {target.name} for {self.attack_power} damage!")
         
+class Player(Character):
+    def __init__(self, name, health, attack_power):
+        super().__init__(name, health, attack_power)
+        self.potions = 3
+    
+    def use_potion(self):
+        if self.potions > 0:
+            self.health += 20
+            self.potions -= 1
+            print(f"{self.name} uses a potion and heals 20 HP!")
+        else:
+            print("No potions left!")
+        
 def main():
-    player = Character("Hero", 100,15)
+    player_name = input("Enter your character name: ")
+    player = Player(player_name, 100,15)
     enemy = Character("Goblin", 60, 10)
     
-    print("Welcome to RPG Battle Game!")
+    
+    print(f"\n Welcome to RPG Battle Game {player_name}!")
     print(f"\n ")
     print(f"{player.name} vs {enemy.name}")
     
     while player.is_alive() and enemy.is_alive():
-        print("\nWhat would you like to do?")
+        print("\nWhat would you like to do? Choose an action:")
         print("1. Attack")
-        print("2. Quit")
+        print("2. Use Potion")
+        print("3. Quit")
         
         choice = input("Choose an option: ")
+        
+        print(f"\n ")
         
         if choice == "1":
             player.attack(enemy)
             
-            if enemy.is_alive():
-                enemy.attack(player)
-        
         elif choice == "2":
+            player.use_potion()         
+        
+        elif choice == "3":
             print("You quit the game.")
             break
         
         else:
-            print("Invalid choice.  Please choose 1 or 2.")
+            print("Invalid choice. Try again. 1, 2 or 3")
+            continue
+        
+        if enemy.is_alive():
+            enemy.attack(player)
             
-        print(f"\n{player.name} Health: {player.health}")
+        print(f"\n{player.name} Health: {player.health} | Potions:{player.potions}")
         print(f"\n{enemy.name} Health: {enemy.health}")
     
     if player.is_alive() and not enemy.is_alive():
-        print("\nYou defeated the enemy!")
+        print("\n 🎉 You defeated the enemy!")
     elif enemy.is_alive() and not player.is_alive():
-        print("\nYou were defeated.")
+        print("\n 💀 You were defeated.")
         
 main()
